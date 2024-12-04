f63559e6b7c133aa41b34a11d208817ec74d7801
Yogesh Dadas <114487898+yogesh-dadas017@users.noreply.github.com>
2024-12-05 01:57:15 +0530

    ok
    ok

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MainPage from './components/MainPage';
+
+
+const App = () => {
+  return (
+    <div>
+      <MainPage/>
+    </div>
+  );
+}
+
+export default App;
