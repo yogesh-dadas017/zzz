c25ae57e7a776efae3b34815011fad928332995a
Yogesh Dadas <114487898+yogesh-dadas017@users.noreply.github.com>
2024-12-05 18:03:17 +0530

    Add files via upload

---
+++ 5-12/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import MainPage from './components/MainPage';
+import PageOne from './components/PageOne';
+import Navbar from './components/Navbar';
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
